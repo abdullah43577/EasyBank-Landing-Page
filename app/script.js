238f7d8e36ff9c12dbe4b2de0ed5c0c9757a1d3e
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-19 22:16:24 +0100

    done with the challenge

--- app/script.js
+++ app/script.js
@@ -3,11 +3,14 @@
 class Nav {
   constructor() {
     this.burger = document.querySelector(".burger");
+    this.listItems = document.querySelector(".list--items");
     this.burger.addEventListener("click", this.openMenu.bind(this));
   }
 
   openMenu() {
     console.log("clicked");
+    this.listItems.classList.toggle("show");
+    this.burger.classList.toggle("toggle");
   }
 }
 

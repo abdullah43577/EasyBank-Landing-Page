13ae6cdf2ab940d0f5748fc384f84a04906c113c
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-19 08:56:06 +0100

    still working on the project, nearly done with the UI

--- app/script.js
+++ app/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+class Nav {
+  constructor() {
+    this.burger = document.querySelector(".burger");
+    this.burger.addEventListener("click", this.openMenu.bind(this));
+  }
+
+  openMenu() {
+    console.log("clicked");
+  }
+}
+
+const nav = new Nav();
